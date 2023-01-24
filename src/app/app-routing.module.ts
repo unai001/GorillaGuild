8fcbe612d1d4a9684fe4663de2589e3aba282f10
UnaiManzanoPerez <79275029+UnaiManzanoPerez@users.noreply.github.com>
2023-01-24 19:48:57 +0100

    nj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import {BroadcastsComponent} from "./broadcasts/broadcasts.component";
 import {TournamentsComponent} from "./tournaments/tournaments.component";
 import {Plataforma14Component} from "./plataforma14/plataforma14.component";
+import {MainPageComponent} from "./main-page/main-page.component";
 
 const routes: Routes = [
+  { path: '', component: MainPageComponent },
   { path: 'broadcasts', component: BroadcastsComponent },
   { path: 'torneos', component: TournamentsComponent },
   { path: 'plataforma14', component: Plataforma14Component }
